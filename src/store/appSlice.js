9e99e4b04abefcff389b9cbc7c8867614919893c
HShaw215 <hu.shaw215@gmail.com>
2023-06-26 15:49:30 -0400

    configured state and reducers

---
+++ src/store/appSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    features: {},
+    tests: {},
+    loggedIn: false,
+    loginModal: closed,
+    signupModal: closed,
+    featureModal: closed,
+    testModal: closed
+};
+
+const appSlice = createSlice({
+    name: 'app',
+    initialState,
+    reducers: {
+        setFeatures: (state, action) => {
+            state.features = action.payload;
+        },
+        setTests: (state, action) => {
+            state.tests = action.payload;
+        },
+        setLoggedIn: (state, action) => {
+            state.loggedIn = action.payload;
+        },
+        setLoginModal: (state, action) => {
+            state.loginModal = action.payload;
+        },
+        setSignupModal: (state, action) => {
+            state.signupModal = action.payload;
+        },
+        setFeatureModal: (state, action) => {
+            state.featureModal = action.payload;
+        },
+        setTestModal: (state, action) => {
+            state.testModal = action.payload;
+        },
+    },
+});
+
+export const { setFeatures, setTests, setLoggedIn, setLoginModal, setSignupModal, setFeatureModal, setTestModal } = appSlice.actions;
+
+export default appSlice.reducer
+
