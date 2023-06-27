d8eeae6521356cc912888dea1b1db36bebff5dbc
pearlhchang <p.chang1802@gmail.com>
2023-06-27 12:25:01 -0700

    update database

--- server/models/testTrackerModel.js
+++ server/models/testTrackerModel.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const PG_URI =
-  "postgres://ohrndtor:fguMG3DeFLWz5mdIsTspVdwR7Hy9_N3O@rajje.db.elephantsql.com/ohrndtor";
+  "postgres://gfnkxmtf:Is08Be6qFLqqe3_vQ-K8KTaqIMV07Ee2@rajje.db.elephantsql.com/gfnkxmtf";
 
 // create a new pool here using the connection string above
 const pool = new Pool({
@@ -19,7 +19,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE feature (
-	"_id" varchar primary key,
+	"_id" serial primary key,
 	"user_id" bigint references users("_id"),
 	"feature_name" varchar,
 	"description" varchar,
@@ -27,8 +27,8 @@ CREATE TABLE feature (
 );
 
 CREATE TABLE test (
-	"_id" varchar primary key,
-	"feature_id" varchar references feature("_id"),
+	"_id" serial primary key,
+	"feature_id" bigint references feature("_id"),
 	"test_name" varchar,
 	"description" varchar,
 	"status" varchar,
