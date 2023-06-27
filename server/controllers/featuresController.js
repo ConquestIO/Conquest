dde7342fefb5467dcd3fc2556a9d149327563ef0
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-27 13:52:58 -0500

    completed featuresController

--- server/controllers/featuresController.js
+++ server/controllers/featuresController.js
@@ -1,4 +1,4 @@
-import db from '../models/testTrackerModel.js'
+import db from '../models/testTrackerModel.js';
 
 const featuresController = {
   getFeaturesList: async (req, res, next) => {
@@ -6,21 +6,16 @@ const featuresController = {
 
     const { userId } = req.user;
 
-    // const userId  = 1; // for testing
-
     try {
-      const query = `SELECT _id AS id, feature_name, description, created_on FROM feature WHERE feature.user_id = $1;`;
+      const query = `SELECT _id AS id, feature_name, description, created_on FROM feature WHERE feature.user_id = $1`;
 
       const data = await db.query(query, [userId]);
+      // console.log('data.rows', data.rows);
 
-      console.log('data.rows', data.rows);
-      // validate data
+      // validate data before storage
       if (!data.rows.length) throw new Error;
-
       res.locals.featuresList = await data.rows;
-
       return next();
-
     } catch (err) {
       return next({
         log: `Error in getFeaturesList controller method: ${err}`,
@@ -34,7 +29,30 @@ const featuresController = {
   createFeature: async (req, res, next) => {
     console.log('<--featuresController - createFeature is invoked-->');
 
-    return next();
+    const { featureName, description } = req.body;
+    const { userId } = req.user;
+
+    // TODO: update DB to allow _id be non-null & remove _id from query & values
+
+    try {
+      const query = `INSERT INTO feature(_id, feature_name, description, user_id) VALUES ($1, $2, $3, $4) RETURNING *;`;
+
+      const values = [20, featureName, description, userId];
+
+      const newFeature = await db.query(query, values);
+      console.log('newFeature.rows', newFeature.rows);
+
+      // validate data before storage
+      if (!newFeature.rows.length) throw new Error;
+      res.locals.newFeature = await newFeature.rows[0];
+      return next();
+    } catch (err) {
+      return next({
+        log: `Error in createFeature controller method: ${err}`,
+        status: 500,
+        message: 'Error while adding data',
+      });
+    }
   }
 
 };
