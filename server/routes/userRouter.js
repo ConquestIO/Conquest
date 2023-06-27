5527bd226b9e2077ea2323d79bbea9f3aa3197cd
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-27 18:05:08 -0500

    removed return .json

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,7 +4,7 @@ import userController from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.registerUser, (req, res) => {
-  return res.status(201).json(res.locals.user);
+  return res.sendStatus(201);
 });
 
 userRouter.post("/login", userController.loginUser, (req, res) => {
