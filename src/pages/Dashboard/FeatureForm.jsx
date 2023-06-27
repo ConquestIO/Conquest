38f5803c389b689617f0144c9fb72e4e3d05a12a
HShaw215 <hu.shaw215@gmail.com>
2023-06-27 13:45:01 -0400

    button rendering for feature modal

--- src/pages/Dashboard/FeatureForm.jsx
+++ src/pages/Dashboard/FeatureForm.jsx
@@ -69,28 +69,28 @@ import Dropdown from 'react-bootstrap/Dropdown'
                   spellCheck='false'
                 />
               </div>
-              <div className='mb-6'>
-                <Dropdown>
+              <div className='mb-6 bg-blue'>
+                <Dropdown className= 'bg-black px-3 py-2'>
                     <Dropdown.Toggle variant='success' id='dropdown-basic'>
                         Select A Status
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href='#/action1' >Not Started</Dropdown.Item>
-                        <Dropdown.Item href='#/action2' >In Progress</Dropdown.Item>
-                        <Dropdown.Item href='#/action3' >Functional Tests</Dropdown.Item>
-                        <Dropdown.Item href='#/action3' >E2E Tests</Dropdown.Item>
+                        <Dropdown.Item href='#/action1' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col' >Not Started</Dropdown.Item>
+                        <Dropdown.Item href='#/action2' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>In Progress</Dropdown.Item>
+                        <Dropdown.Item href='#/action3' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>Functional Tests</Dropdown.Item>
+                        <Dropdown.Item href='#/action4' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>E2E Tests</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
               </div>
               <div className='mb-6'>
-              <Dropdown>
+              <Dropdown className='bg-black px-3 py-2 mt-1'>
                     <Dropdown.Toggle variant='success' id='dropdown-basic'>
                         Select A Category
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item href='#/action1' >Unit Tests</Dropdown.Item>
-                        <Dropdown.Item href='#/action2' >Integration Tests</Dropdown.Item>
-                        <Dropdown.Item href='#/action3' >Completed</Dropdown.Item>
+                        <Dropdown.Item href='#/action1' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>Unit Tests</Dropdown.Item>
+                        <Dropdown.Item href='#/action2' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>Integration Tests</Dropdown.Item>
+                        <Dropdown.Item href='#/action3' className=' focus:shadow-outline w-full appearance-none rounded border bg-sky-700 px-3 py-2 flex flex-col'>Completed</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
               </div>
