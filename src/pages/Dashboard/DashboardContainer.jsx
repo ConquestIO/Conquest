5286ae4fd07337db28869fb2fa9f96f0179797d6
HShaw215 <hu.shaw215@gmail.com>
2023-06-26 23:00:56 -0400

    set up fetch to populate features state

--- src/pages/Dashboard/DashboardContainer.jsx
+++ src/pages/Dashboard/DashboardContainer.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { setFeature, } from '../../store/appSlice';
 import TestDisplay from './TestDisplay';
 import SideBarContainer from './SideBarContainer';
 
-return (
-    <div>
-        <SideBarContainer />
-        <TestDisplay />
-    </div>
-)
+export default function App(){
+    const dispatch = useAppDispatch();
+    const userID = useAppSelector((state) => state.app.userID)
+
+    //on render, fetch all features associated with user and update state
+    useEffect(() => {
+        (async () => {
+            await fetch(`/api/${userID}/features`)
+            .then((res) => dispatch(setFeature(res)))
+        })
+    })
+
+    return (
+        <div>
+            <SideBarContainer />
+            <TestDisplay />
+        </div>
+    );
+}
