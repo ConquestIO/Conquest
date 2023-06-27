0ebc132e3e63121c294c63583e481ccbcfffabb6
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-27 13:58:39 -0500

    added comments

--- server/controllers/featuresController.js
+++ server/controllers/featuresController.js
@@ -1,6 +1,7 @@
 import db from '../models/testTrackerModel.js';
 
 const featuresController = {
+  // get full list of features from DB for specific user
   getFeaturesList: async (req, res, next) => {
     console.log('<--featuresController - getFeaturesList is invoked-->');
 
@@ -9,8 +10,9 @@ const featuresController = {
     try {
       const query = `SELECT _id AS id, feature_name, description, created_on FROM feature WHERE feature.user_id = $1`;
 
-      const data = await db.query(query, [userId]);
-      // console.log('data.rows', data.rows);
+      const values = [userId];
+
+      const data = await db.query(query, values);
 
       // validate data before storage
       if (!data.rows.length) throw new Error;
@@ -26,25 +28,24 @@ const featuresController = {
 
   },
 
+  // create a new feature in DB for specific user
   createFeature: async (req, res, next) => {
     console.log('<--featuresController - createFeature is invoked-->');
 
     const { featureName, description } = req.body;
     const { userId } = req.user;
 
-    // TODO: update DB to allow _id be non-null & remove _id from query & values
+    // TODO: update DB to allow _id be non-null & remove _id query & 25 from values
 
     try {
       const query = `INSERT INTO feature(_id, feature_name, description, user_id) VALUES ($1, $2, $3, $4) RETURNING *;`;
 
-      const values = [20, featureName, description, userId];
+      const values = [25, featureName, description, userId];
 
       const newFeature = await db.query(query, values);
-      console.log('newFeature.rows', newFeature.rows);
 
-      // validate data before storage
-      if (!newFeature.rows.length) throw new Error;
-      res.locals.newFeature = await newFeature.rows[0];
+      // validate wehtehr new Feature is successfully added to DB
+      if (!newFeature.rows[0]) throw new Error;
       return next();
     } catch (err) {
       return next({
