6ba71eed8b920110c862ad093c4982794c11ca1a
pearlhchang <p.chang1802@gmail.com>
2023-06-26 21:51:57 -0700

    add auth

--- server/controllers/authController.js
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
-const db = require("../models/testTrackerModel");
-
-const authenticateController = {};
-
-module.exports = authenticateController;
