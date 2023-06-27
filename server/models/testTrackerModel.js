86a326ac05979fdeda138d9455caa0dbb7c786e7
pearlhchang <p.chang1802@gmail.com>
2023-06-26 21:34:47 -0700

    update file name

--- server/models/testTrackerModel.js
+++ server/models/testTrackerModel.js
@@ -38,9 +38,6 @@ CREATE TABLE test (
 
 */
 
-// We export an object that contains a property called query,
-// which is a function that returns the invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
 module.exports = {
   query: (text, params, callback) => {
     console.log("executed query", text);
