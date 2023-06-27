a4ce0e672d163ad9cdec50ffc09a6107925258a6
Julian Babon <babonjmc@gmail.com>
2023-06-27 01:23:42 -0400

    initial landing page completed

--- src/pages/Home/Intro.jsx
+++ src/pages/Home/Intro.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const Intro = () => {
     return (
-    <div className="min-h-screen flex justify-center items-center bg-bg2 bg-fixed bg-no-repeat bg-cover bg-center">
-      <div className="flex justify-end items-end w-3/4 mt-20">
-        <h1 className="font-extrabold text-8xl font-serif">Conquest</h1>
+    <div className='min-h-screen flex justify-center items-center bg-bg2 bg-fixed bg-no-repeat bg-cover bg-center'>
+      <div className='flex justify-end items-end w-3/4 mt-20'>
+        <h1 className='font-extrabold text-8xl font-serif'>Conquest</h1>
       </div>
     </div>
     )
--- src/pages/Home/Summary.jsx
+++ src/pages/Home/Summary.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Summary = () => {
   return (
     <>
-    <div className="p-10 bg-gray-900 text-center">
-      <h2 className="font-extrabold text-4xl text-white leading-tight">Gain Clarity and Insight into Testing Coverage</h2>
+    <div className='p-10 bg-gray-900 text-center'>
+      <h2 className='font-extrabold text-4xl text-white leading-tight'>Gain Clarity and Insight into Testing Coverage</h2>
       <p className='text-white mt-4 text-lg'>Track Your Testing Efforts for Every Feature with Our Web App!</p>
-      <div className="flex justify-center items-center gif-container">
-          <img src="https://media.tenor.com/5aF7np_zPEgAAAAM/pepe-why-pepe-the-frog.gif" alt="GIF" className="w-64 h-64" />
+      <div className='flex justify-center items-center gif-container'>
+          <img src='https://media.tenor.com/5aF7np_zPEgAAAAM/pepe-why-pepe-the-frog.gif' alt='GIF' className='w-64 h-64' />
         </div>   
     </div>
-    <div className="bg-bg1 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed"></div>
+    <div className='bg-bg1 min-h-screen bg-center bg-no-repeat bg-cover bg-fixed'></div>
     </>
   )
 };
