3b860a9ab87f46ee127442cca8c55ba9139000ae
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-27 20:05:49 -0500

    minor changes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,9 +3,7 @@ import userController from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userController.registerUser, (req, res) => {
-  return res.sendStatus(201);
-});
+userRouter.post("/register", userController.registerUser, userController.loginUser, (req, res) => res.status(201).json(res.locals.user));
 
 userRouter.post("/login", userController.loginUser, (req, res) => {
   return res.status(200).json(res.locals.user);
