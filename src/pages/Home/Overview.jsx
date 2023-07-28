e2cfbe3d25023f7e4a042c2acc25cf026967cda7
HShaw215 <hu.shaw215@gmail.com>
2023-07-28 16:05:07 -0400

    fixed conquest title spacing

--- src/pages/Home/Overview.jsx
+++ src/pages/Home/Overview.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Overview = () => {
     return (
-    <div id="overview" className='min-h-screen flex justify-center items-center bg-bg2 bg-fixed bg-no-repeat bg-cover bg-center'>
-      <div className='flex justify-end items-end w-3/4 mt-20'>
-        <h1 className='font-extrabold text-8xl font-serif pb-14'>Conquest</h1>
+    <div id="overview" className='min-h-screen flex justify-center items-center bg-bg2 bg-no-repeat bg-cover bg-center'>
+      <div className='pl-96 ml-96 absolue pt-16'>
+        <h1 className='font-extrabold text-8xl font-serif'>Conquest</h1>
       </div>
     </div>
     )
